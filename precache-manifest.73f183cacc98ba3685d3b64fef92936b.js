91a40265ed38b406937746c9e4e059058d45cef4
Rosa <rosaislp@gmail.com>
2020-07-31 14:27:32 -0500

    Updates

---
+++ precache-manifest.73f183cacc98ba3685d3b64fef92936b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84ae282a09c62cbfa93c9d5a277f2f40",
+    "url": "/lapieza-challenge/index.html"
+  },
+  {
+    "revision": "89e37ddcc92fd06ac224",
+    "url": "/lapieza-challenge/static/css/main.bc116ea4.chunk.css"
+  },
+  {
+    "revision": "81e764ee3783b951b25c",
+    "url": "/lapieza-challenge/static/js/2.064a59a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lapieza-challenge/static/js/2.064a59a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89e37ddcc92fd06ac224",
+    "url": "/lapieza-challenge/static/js/main.b69ad67a.chunk.js"
+  },
+  {
+    "revision": "fd71b56cef535b8d17de",
+    "url": "/lapieza-challenge/static/js/runtime-main.335f5d25.js"
+  },
+  {
+    "revision": "cc77cb5f2c003838c5e42d7818082f5b",
+    "url": "/lapieza-challenge/static/media/background.cc77cb5f.jpg"
+  }
+]);
