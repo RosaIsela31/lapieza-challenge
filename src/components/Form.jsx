938bda84447f89a95b6064f4af0be0732d04f169
Rosa <rosaislp@gmail.com>
2020-07-31 13:39:25 -0500

    adding conditional to show the form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,64 @@
 import React, { useState } from 'react';
+import styled from "styled-components";
+import { FiX } from "react-icons/fi";
+
+import '../App.css'
+
+
+const Button = styled.button`
+  background-color: #F73552;
+  color: #FFF;
+  border: 0;
+  height: 4rem;
+  width: 4rem;
+  border-radius: 50px;
+  outline: none;
+`;
+
+const Forms = styled.form`
+  display: grid;
+  grid-template-columns: 85% 15%;
+  width: 80%;
+`;
+
+const InputText = styled.input`
+  width: 85%;
+  margin-bottom: 1rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  height: 2.5rem;
+  padding-left: 2rem;
+  border-radius: 5px;
+  outline: none;
+  border: 0;
+  box-shadow: 
+        0.1em 0.1em 0.1em 0.1em rgba(0,0,0,0.1), 
+             0 0  0 1px rgb(255,255,255),
+             0.1em 0.1em 1em rgba(0,0,0,0.1);
+  font-family: 'Montserrat', sans-serif;
+  
+`;
+
+const TextArea = styled.textarea`
+  width: 85%;
+  margin-bottom: 1rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  height: 5rem;
+  padding-left: 2rem;
+  padding-top: 1rem;
+  border-radius: 5px;
+  outline: none;
+  border: 0;
+  box-shadow: 
+        0.1em 0.1em 0.1em 0.1em rgba(0,0,0,0.1), 
+             0 0  0 1px rgb(255,255,255),
+             0.1em 0.1em 1em rgba(0,0,0,0.1);
+  font-family: 'Montserrat', sans-serif;
+  resize: none;
+`;
 
 const Form = () => {
   
@@ -32,25 +92,29 @@ const Form = () => {
   const { title, body } = info;
   
   return(
-    <form
+    <Forms
       onClick={submitUserInfo}
     >
-      <input 
-        name='value'
-        placeholder='title'
-        value={title}
-        onChange={handleTextChange}
-      />
+      <div>
+        <InputText 
+          name='value'
+          placeholder='Title'
+          value={title}
+          onChange={handleTextChange}
+        />
+        
+        <TextArea 
+          name='body'
+          placeholder='Body'
+          value={body}
+          onChange={handleTextChange}
+        /> 
+      </div>
       
-      <input 
-        name='body'
-        placeholder='body'
-        value={body}
-        onChange={handleTextChange}
-      /> 
-      
-      <input type='submit' value='enviar' />
-    </form>
+      <Button type='submit' value='enviar' > 
+        <FiX className='fi-x'/>
+      </Button>
+    </Forms>
   )
 }
 
